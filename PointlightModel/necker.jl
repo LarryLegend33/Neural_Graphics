1c1bb8147dde8d57257ad6e92b4a61b6fd0d0089
Andrew Bolton <andrewdbolton@fas.harvard.edu>
2021-04-24 22:33:25 -0400

    figured out rendering of cubes and wireframes from Rect objects

--- PointlightModel/necker.jl
+++ PointlightModel/necker.jl
@@ -20,18 +20,50 @@ N = 100
 #     rotations = Vec3f0.(rand(N) .* 0.3, rand.() .* 0.8, 1) # also accepts Quaternions
 # )
 
-# fig, ax = mesh(
-#     Rect(Vec(0, 0, 0), Vec(1, 1, 1)), color = :skyblue2,
-# )
+
+# constructor has to be with floats to use quaternion rotation w/ floats. q * p
+
+cube_prim = Rect(Vec(0.0, 0.0, 0.0), Vec(1.0, 1.0, 1.0))
+cube_mesh = GeometryBasics.mesh(cube_prim)
+vertices = cube_mesh.position
+rotator = Quaternionf0(0, -.2, 0, 1)
+
+# RENDER THE WIREFRAMES AND MESH PLOTS DIRECTLY FROM CUBE_PRIM.
+# TRANSFORM POSITIONS USING [rotator * p for p in cube_mesh.position]
+# use 
+
+
+fig, ax = mesh(
+    cube_mesh, color = :skyblue2,
+)
+
+
+
+ 
+cube = ax.scene[end]
+GLMakie.rotate!(cube, rotator)
+
+rotated_positions = [rotator * p for p in cube_mesh.position]
+
+# once you've appled the same rotations to the mesh in the plot and the
+
+
+# there is a 3 column matrix that comes out of this that changes w rotation. 
+println(cube.model)
+# cube_mesh.position yields the unrotated vertices
+# cube.transformation.rotation gives you the rotation applied.
+# cube.transformation.scale gives you scaling
+# 
+
+
 
 
 #ax.scene[end] is the rectangle. you can use this in the rotation function rotate! and the axis will stay the same
 # but the cube will rotate. 
 
 
-
 fig, ax = mesh(
- FRect3D(Vec3f0(-0.5), Vec3f0(1)), color = :skyblue2,
+ Rect(Vec3f0(-0.5), Vec3f0(1)), color = :skyblue2,
 )
 
 # ax.scene.camera contains the projection matricies w/ .projeciton and .projectionview
@@ -45,6 +77,8 @@ two_d_grid = AbstractPlotting.colorbuffer(screen)
 
 # GLMakie.scene2image(ax.scene)
 
+# apparently you can also disable the rendering loop to make colorbuffer faster
+# GLMakie.opengl_renderloop[] = (screen) -> nothing. would have to test this. 
 
 
 
