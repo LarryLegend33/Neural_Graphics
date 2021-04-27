df8cd6a575d50af634295b51a6be480a7853703c
Andrew Bolton <andrewdbolton@fas.harvard.edu>
2021-04-27 00:06:01 -0400

    working towards necker animation

--- PointlightModel/necker.jl
+++ PointlightModel/necker.jl
@@ -10,27 +10,9 @@ using GeometryBasics
 # note there are residual types in Makie from GeometryTypes, which is
 # deprecated in favor of GeometryBasics
 
-N = 100
-
-# m = meshscatter(
-#     Point3f0.(rand.() .* 4, rand(N) .* 4, rand.() .* 0.01),
-#     markersize = Vec3f0.(rand.() .* 0.3, rand.() .* 0.3, rand(N)), 
-#     marker = FRect3D(Vec3f0(0), Vec3f0(1)),
-#     color = rand(RGBf0, N),
-#     rotations = Vec3f0.(rand(N) .* 0.3, rand.() .* 0.8, 1) # also accepts Quaternions
-# )
-
-
-# constructor has to be with floats to use quaternion rotation w/ floats. q * p
-
-cube_prim = GeometryBasics.Rect(Vec(0.0, 0.0, 0.0), Vec(1.0, 1.0, 1.0))
 
 # can also get positions w/out going to mesh first if you want to. i.e. vertices = decompose(Point3, cube_prim)
 # can construct a primitive as well w/ a set of vertices (i.e. 
-
-
-#cube_mesh = GeometryBasics.mesh(cube_prim)
-#cube_mesh.position
 
 vertices = decompose(Point{3, Float64}, cube_prim)
 # list of vertices
@@ -42,90 +24,133 @@ cube_mesh = GeometryBasics.Mesh(vertices, faces)
 z_rotator = qrotation(Vec3f0(0, 0, 1), -.5)
 x_rotator = qrotation(Vec3f0(1, 0, 0), -.5)
 rotator = z_rotator * x_rotator
-
-
-#rotator = Quaternionf0(0, -.2, 0, 1)
-
 rotated_verts = [rotator * v for v in vertices]
 rotated_mesh = GeometryBasics.Mesh(rotated_verts, faces)
 
-fig, ax = GLMakie.mesh(rotated_mesh, color=:skyblue2)
+struct Rotator
+    euler_axis::Vec
+    rot_radians::Float64
+end
+    
+cube_prim = GeometryBasics.Rect(Vec(-.5, -.5, -.5), Vec(1.0, 1.0, 1.0)) # origin and side size
+sphere_prim = GeometryBasics.Sphere(Point3(0.0, 0.0, 0.0), 1) # origin, radius
+cylinder_prim = Cylinder(Point3(0.0, 0.0, 0.0), Point3(0.0,0.0,1.0), 1.0) #(origin, normal vector, width)
+shape_types = [cube_prim, sphere_prim, cylinder_prim]
+@dist choose_shape() = shape_types[categorical([1/3, 1/3, 1/3])]
 
-# qrotate will make a quaternion out of a set of rotations
-# 
 
-# RENDER THE WIREFRAMES AND MESH PLOTS DIRECTLY FROM CUBE_PRIM.
-# TRANSFORM POSITIONS USING [rotator * p for p in cube_mesh.position]
-# use 
+@gen function primitive_shapes()
+    shape = { :shape_choice } ~ choose_shape()
+    rotation_x = { :rot_x } ~ uniform(0, π)
+    rotation_y = { :rot_y } ~ uniform(0, π)
+    rotation_z = { :rot_z } ~ uniform(0, π / 2)
+    
+    
+    mesh_to_grid = scene2image(mesh_ax.scene)[1]
+    noisy_image = ({ :image_2D } ~ noisy_matrix(blurred_depth_image, 0.1))
+end
+
+function render_static_wireframe(mesh, rotation::Rotator, mesh_or_wire::String)
+    white = RGBAf0(255, 255, 255, 0.0)
+    if mesh_or_wire == "wire"
+        mesh_fig, mesh_axis = GLMakie.wireframe(cube_prim, color=:skyblue2)
+    elseif mesh_or_wire == "mesh"
+        mesh_fig, mesh_axis = GLMakie.mesh(cube_prim, color=:skyblue2)
+    end
+    meshscene = mesh_axis.scene[end]
+    screen = display(mesh_fig)
+    remove_axis_from_scene(mesh_axis)
+    rotate!(meshscene, qrotation(rotation.euler_axis, rotation.rot_radians))
+    return mesh_axis
+end    
+    
+
+function animate_mesh_rotation(mesh, rotations)
+    time_node = Node(1);
+  
+    f(t, rotations) = qrotation(rotations[t]...)
+    mesh_fig, mesh_axis = GLMakie.wireframe(cube_prim, color=:skyblue2)
+    meshscene = mesh_axis.scene[end]
+    # another option is rotating outside the "rotations" call and instead lifting the mesh itself.
+    # then all of your rotations are on the mesh instead of the 
+    screen = display(mesh_fig)
+    remove_axis_from_scene(mesh_axis)
+    # threeDaxis = mesh_axis.scene[OldAxis]
+    # threeDaxis[:showgrid] = (false, false, false)
+    # threeDaxis[:showaxis] = (false, false, false)
+    # threeDaxis[:ticks][:textcolor] = (white, white, white)
+    # threeDaxis[:names, :axisnames] = ("", "", "")
+    for r in rotations
+        rotate!(meshscene, qrotation(r...))
+        # need a 2D gridsave in the loop
+        sleep(.1)
+    end
+    return mesh_axis
+end
+
+
+function remove_axis_from_scene(mesh_axis)
+    white = RGBAf0(255, 255, 255, 0.0)
+    threeDaxis = mesh_axis.scene[OldAxis]
+    threeDaxis[:showgrid] = (false, false, false)
+    threeDaxis[:showaxis] = (false, false, false)
+    threeDaxis[:ticks][:textcolor] = (white, white, white)
+    threeDaxis[:names, :axisnames] = ("", "", "")
+    return threeDaxis
+end
+
+
 
     
-# w Rotations.jl, can make a Quat(q.data...) call to get a quat
-    
-# fig, ax = mesh(
-#     cube_prim, color = :skyblue2,
-# )
- 
-cube = ax.scene[end]
-GLMakie.rotate!(cube, rotator)
+struct NoisyMatrix <: Gen.Distribution{Matrix{Float64}} end
 
-#rotated_positions = [rotator * p for p in cube_mesh.position]
+const noisy_matrix = NoisyMatrix()
 
-# once you've appled the same rotations to the mesh in the plot and the
+function Gen.logpdf(::NoisyMatrix, x::Matrix{Float64}, mu::Matrix{U}, noise::T) where {U<:Real,T<:Real}
+    var = noise * noise
+    diff = x - mu
+    vec = diff[:]
+    return -(vec' * vec)/ (2.0 * var) - 0.5 * log(2.0 * pi * var)
+end
+
+function Gen.random(::NoisyMatrix, mu::Matrix{U}, noise::T) where {U<:Real,T<:Real}
+    mat = copy(mu)
+    (w, h) = size(mu)
+    for i=1:w
+        for j=1:h
+            mat[i, j] = mu[i, j] + randn() * noise
+            if mat[i, j] > 1
+                mat[i, j] = 1
+            elseif mat[i, j] < 0
+                mat[i, j] = 0
+            end
+        end
+    end
+    return mat
+end
 
 
-# there is a 3 column matrix that comes out of this that changes w rotation. 
-println(cube.model)
+""" NOTES """
 # cube_mesh.position yields the unrotated vertices
 # cube.transformation.rotation gives you the rotation applied.
 # cube.transformation.scale gives you scaling
-# 
-
 # qrotation takes an axis (e.g. Vec3(1,0,0)) and a radian angle and returns a Quaternion. 
-
-
-#ax.scene[end] is the rectangle. you can use this in the rotation function rotate! and the axis will stay the same
-# but the cube will rotate. 
-
-
-
-
+# w Rotations.jl, can make a Quat(q.data...) call to get a quaternion, which can be multiplied by 3D vecs
+# coordinates(shape, nvertices=2) returns 
 # ax.scene.camera contains the projection matricies w/ .projeciton and .projectionview
+# lift syntax for animating a meshscatter
 
-screen = display(ax.scene)
-two_d_grid = AbstractPlotting.colorbuffer(screen)
+    # mesh_fig, mesh_ax = meshscatter(
+    #     Point3f0.(rand.() .* 4, rand(N) .* 4, rand.() .* 0.01),
+    #     markersize = Vec3f0.([0, .3], [0, .3], [0, 1.0]), 
+    #     marker = cube_prim, 
+    #     color = :skyblue2,
+    #     rotations =  lift(t -> f(t, rotations), time_node),
+    #     ssao = true)
 
-    
-
-# THIS REQUIRES DISPLAY |^
-
-# This doesnt
-
-
-
-#can also
-
-
-# This completely works    
-ax.scene.center = false
-im = GLMakie.scene2image(ax.scene)
-    # EQUIVALENT
-save("test.png", ax.scene)
-save("test2.png", im[1])
-
-
-
-# apparently you can also disable the rendering loop to make colorbuffer faster
-# GLMakie.opengl_renderloop[] = (screen) -> nothing. would have to test this. 
-
-
-
-
-
-# to get a 2D grid out of the campixel
-
-
-#rect = scene[end] # last plot is the rect
-# there are a couple of ratate! functions, that accept e.g. a vector etc
-#rotate!(rect, Quaternionf0(0, 0.4, 0, 1))
-#scene
+# ax.scene.center = false
+# im = GLMakie.scene2image(ax.scene)
+#     # EQUIVALENT
+# save("test.png", ax.scene)
+# save("test2.png", im[1])
 
